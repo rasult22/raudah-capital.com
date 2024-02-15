f984c5745798160b0edd9c4537e5e746c1078e72
Itachi-MacOS <warrior.006@inbox.ru>
2024-02-16 01:49:04 +0600

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   site: "https://rasult22.github.io",
+  base: '/',
   integrations: [tailwind(), icon(), svelte()]
 });
