8cca358b164e400549cd974eef5b5fde105c0ef4
Itachi-MacOS <warrior.006@inbox.ru>
2024-02-16 23:03:38 +0600

    sm: focus, invest, access, about

--- src/components/svelte/accordion.svelte
+++ src/components/svelte/accordion.svelte
@@ -41,20 +41,20 @@ const open = (id) => {
     <h6 class="mb-0">
       <button
         on:click={() => open(item.id)}
-        class="relative text-[19px] flex items-center justify-between w-full p-4 text-left transition-all ease-in border-b border-solid cursor-pointer border-slate-100 text-slate-700 rounded-t-1 group text-dark-500"
+        class="relative text-[19px] sm:text-[16px] flex items-center justify-between w-full p-4 text-left transition-all ease-in border-b border-solid cursor-pointer border-slate-100 text-slate-700 rounded-t-1 group text-dark-500"
         data-collapse-target="collapse-1"
       >
         <span>{item.title}</span>
-        <svg class={item.isOpen ? '' : 'rotate-180'} width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg class={item.isOpen ? 'rotate-180' : ''} width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M20.8147 9.60089L14.2947 16.1209C13.5247 16.8909 12.2647 16.8909 11.4947 16.1209L4.9747 9.60089" stroke="#0080B5" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
       </button>
     </h6>
     <div
       data-collapse="collapse-1"
-      class={`${item.isOpen ? 'h-[145px] overflow-visible' : 'h-0  overflow-hidden'}  transition-all duration-300 ease-in-out`}
+      class={`${item.isOpen ? 'h-[95px] sm:h-[200px] overflow-visible' : 'h-0  overflow-hidden'}  transition-all duration-300 ease-in-out`}
     >
-      <div class="p-4 text-sm text-[15px] leading-normal text-blue-gray-500/80">
+      <div class="p-4 text-[15px] leading-normal text-blue-gray-500/80">
        {item.content}
       </div>
     </div>
